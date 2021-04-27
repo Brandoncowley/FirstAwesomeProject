c2c8353a1fff0204d8dc321d7682e97a71100f18
Brandon Cowley <brandon.cowley@gmail.com>
2021-04-26 20:16:29 -0700

    Adding updated notes to sheets

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //JS sheet begins here
+//ADD APIs HERE
+var requestUrl = 'http://www.boredapi.com/api/activity/';
+
+//SECOND API - pull info from general query
+//SECOND API - insert image that ties to the activity (Unsplash?)
+
+
+//9 different categories, arrange in an array, w/ up to 10 activities being listed
+//insert image that ties to the activity (Unsplash?)
+//Give brief on the activity
+//Data key can give visual representation (icon)
+
+
+//LAYOUT: list, "click here for more information" - give items above
+// - ALSO - possible tie in for musical selection
+    
