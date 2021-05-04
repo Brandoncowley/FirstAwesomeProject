53ef53b7d0497b700c1534d10b36c3304218074c
michaelzadra27 <michaelzadra27@gmail.com>
2021-05-03 19:22:02 -0700

    taking melina's JS on to main JS script

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     getApi();
 
 
+
     //SECOND API - pull info from general query
     //SECOND API - insert image that ties to the activity (Unsplash?)
 
@@ -55,3 +56,125 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var category = 'educational';
+if (category === 'Busywork') {
+    category = 'chores'
+};
+var key = '&client_id=n2fGqHKjWmuL15Ufx1eLf0EYsFiL2psgSMrAcGiciX4'
+function getUnsplashApi(category) {
+    var requestUrl = 'https://api.unsplash.com/search/photos?page=1&query=' + category + key
+    console.log(requestUrl);
+    fetch(requestUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayPhoto(data, category);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert('unable to connect to Unsplash');
+        });
+};
+var displayPhoto = function (photoList, category) {
+    console.log(photoList);
+    var currentPhotoNum = (Math.floor(Math.random()*11));
+    console.log(photoList.results[currentPhotoNum].urls.small);
+    console.log(photoList.results[currentPhotoNum].alt_description);
+}
+var imageContainer = document.getElementById('#image-container')
+var image = document.createElement('img');
+getUnsplashApi(category);
