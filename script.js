c82f8c7744eecfce0e5c1451e0e57695b8f66e01
Melina Boedecker <melinamboedecker@gmail.com>
2021-05-03 20:47:48 -0700

    added image to html

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     //Melina's notes for branch testing
 
 
-})
+});
 
 
 
@@ -147,12 +147,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 
-var category = 'educational';
-if (category === 'Busywork') {
-    category = 'chores'
-};
-var key = '&client_id=n2fGqHKjWmuL15Ufx1eLf0EYsFiL2psgSMrAcGiciX4'
+
+
+
+
 function getUnsplashApi(category) {
+    if (category === 'Busywork') {
+        category = 'chores'
+    };
+    
+    var key = '&client_id=n2fGqHKjWmuL15Ufx1eLf0EYsFiL2psgSMrAcGiciX4'
     var requestUrl = 'https://api.unsplash.com/search/photos?page=1&query=' + category + key
     console.log(requestUrl);
     fetch(requestUrl)
@@ -174,7 +178,13 @@ var displayPhoto = function (photoList, category) {
     var currentPhotoNum = (Math.floor(Math.random()*11));
     console.log(photoList.results[currentPhotoNum].urls.small);
     console.log(photoList.results[currentPhotoNum].alt_description);
+
+    var image = document.getElementById('image-img');
+    var photoUrl =  photoList.results[currentPhotoNum].urls.small;
+    image.src = photoList.results[currentPhotoNum].urls.small;
+    image.alt = photoList.results[currentPhotoNum].alt_description;
 }
-var imageContainer = document.getElementById('#image-container')
-var image = document.createElement('img');
-getUnsplashApi(category);
+
+
+
+getUnsplashApi('Educational');
